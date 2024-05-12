9759f7590c5e3b7a28464ede56106c0f52489b13
Jasimraza Momin <jasimrazamomin@Jasimrazas-MacBook-Air.local>
2024-05-11 22:18:47 -0400

    made website static to freely host it

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,7 @@ import { useState,useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Aos from 'aos';
 import "aos/dist/aos.css";
+import emailjs from 'emailjs-com';
 
 export const Contact = () => {
     const formInitialDetails = {
@@ -14,6 +15,8 @@ export const Contact = () => {
     const [formDetails, setFormDetails] = useState(formInitialDetails);
     const [buttonText, setButtonText] = useState('Send');
     const [status, setStatus] = useState({});
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const userId = process.env.REACT_APP_EMAILJS_USER_ID;
 
     const updateForm = (category,value) => {
         setFormDetails({
@@ -27,25 +30,40 @@ export const Contact = () => {
         Aos.init({duration: 700});
     },[])
 
-    const handleSubmit = async(e) => {
+    // const handleSubmit = async(e) => {
+    //     e.preventDefault();
+    //     setButtonText('Sending...');
+    //     let response = await fetch('http://localhost:3080/contact', {
+    //         method: "POST",
+    //         headers: {
+    //             'Content-Type' : 'Application/json;charset=utf-8'
+    //         },
+    //         body: JSON.stringify(formDetails)
+    //     });
+    //     setButtonText('Send');
+    //     let result = await response.json();
+    //     setFormDetails(formInitialDetails);
+    //     if (result.code === 200) {
+    //         setStatus({success:true, message: 'Message Sent'});
+    //     } else {
+    //         setStatus({success:false, message: 'Something went wrong, you can email me at jasimraza@hotmail.com or jrmomin04@gmail.com'});
+    //     }
+    // }
+
+    function sendEmail(e) {
         e.preventDefault();
         setButtonText('Sending...');
-        let response = await fetch('http://localhost:3080/contact', {
-            method: "POST",
-            headers: {
-                'Content-Type' : 'Application/json;charset=utf-8'
-            },
-            body: JSON.stringify(formDetails)
-        });
-        setButtonText('Send');
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
+        emailjs.send(serviceId, 'template_koyupae', formDetails, userId)
+          .then((result) => {
             setStatus({success:true, message: 'Message Sent'});
-        } else {
+            console.log(result.text);
+          }, (error) => {
             setStatus({success:false, message: 'Something went wrong, you can email me at jasimraza@hotmail.com or jrmomin04@gmail.com'});
-        }
-    }
+            console.log(error.text);
+          });
+        setButtonText('Send');
+        setFormDetails(formInitialDetails);
+      }
 
     return (
         <section className="contact" id="connect">
@@ -54,7 +72,7 @@ export const Contact = () => {
                     <Col>
                     <div data-aos="flip-up">
                         <h2>Send Me A Message</h2>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={sendEmail}>
                             <Row>
                                 <Col sm={6} className="px-1">
                                     <input type="text" value={formDetails['firstName']} placeholder="First Name" onChange={(e) => updateForm('firstName',e.target.value)}/>
